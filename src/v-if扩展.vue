6cad3f17a8115c896f13cf07c13624598ed5577c
王思南 <3313714133@qq.com>
2024-11-27 14:24:19 +0800

    v-if扩展

---
+++ src/v-if扩展.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p v-if="num>550">一本</p>
+        <div v-else-if="num>450">二本</div>
+        <button v-else-if="num>350">三本</button>
+        <h4 v-else>重读</h4>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            num: 60
+        }
+    }
+}
+</script>
+<style>
+
+</style>
